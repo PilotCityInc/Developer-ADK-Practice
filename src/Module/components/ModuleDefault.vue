6cbe5f4de9f8e07da2db1019171219db51bde231
Derick Lee <dericklee@pilotcity.com>
2021-03-14 14:56:22 -0700

    1.0.5

--- src/Module/components/ModuleDefault.vue
+++ src/Module/components/ModuleDefault.vue
@@ -1,6 +1,6 @@
 <template>
   <ValidationObserver v-slot="{ invalid }" slim>
-  <div class="module-default">
+  <v-container class="module-default pa-0">
     <div class="module-default__instructions">
       <v-expansion-panels v-model="showInstructions" class="module-default__instructions" flat>
         <v-expansion-panel>
@@ -106,7 +106,7 @@
       <!-- DESIGN YOUR ACTIVITY HERE / COMMENT OUT WHEN YOU'VE STARTED DESIGNING -->
       <!-- <div class="module-default__none">Design your activity here</div> -->
     </div>
-  </div>
+  </v-container>
   </ValidationObserver>
 </template>
 
