cb8f444cdbf7ec1d47ce914233032c430e6c987f
Marcus123u <mightyboy123u@gmail.com>
2021-04-04 20:09:18 +0000

    added presets and default required minutes

--- src/Module/components/ModulePresets.vue
+++ src/Module/components/ModulePresets.vue
@@ -39,51 +39,51 @@
         <!-- </div>
         </v-container> -->
       </ValidationObserver>
-      <!-- <v-divider class="presets__divider"></v-divider>
-      <div class="presets__section-title">Instructions</div>
-      <Instruct v-model="setupInstructions" />
+      <!-- <v-divider class="presets__divider"></v-divider> -->
+      <!-- <div class="presets__section-title">Instructions</div> -->
+      <!-- <Instruct v-model="setupInstructions" /> -->
       <v-divider class="presets__divider"></v-divider>
       <div class="presets__section-title">Defaults</div>
 
       <v-select
-        v-model="adkData.defaultActivity.groupActivity"
+        v-model="programDoc.data.adks[index].defaultActivity.groupActivity"
         disabled
         :items="group"
         label="What activity group does this belong to?"
         outlined
       ></v-select>
       <v-select
-        v-model="adkData.defaultActivity.requiredActivity"
+        v-model="programDoc.data.adks[index].defaultActivity.requiredActivity"
         disabled
         :items="required"
         label="Is this activity required for participants to complete?"
         outlined
-      ></v-select> -->
+      ></v-select>
 
       <!-- <v-select
         :items="lockOrder"
         label="Lock activity group and placement order?"
         outlined
       ></v-select> -->
-      <!-- <v-select
-        v-model="adkData.defaultActivity.deliverableActivity"
+      <v-select
+        v-model="programDoc.data.adks[index].defaultActivity.deliverableActivity"
         disabled
         :items="deliverable"
         label="Is this a deliverable?"
         outlined
-      ></v-select> -->
+      ></v-select>
       <!-- <v-select
         :items="accessibility"
         label="Make this activity accessible to participants anytime?"
         outlined
       ></v-select> -->
-      <!-- <v-select
-        v-model="adkData.defaultActivity.endEarlyActivity"
+      <v-select
+        v-model="programDoc.data.adks[index].defaultActivity.endEarlyActivity"
         disabled
         :items="endEarly"
         label="Allow participants to end program early after completion of this activity?"
         outlined
-      ></v-select> -->
+      ></v-select>
       <!-- POST-ACTIVITY REFLECTION -->
       <!-- <v-text-field
         label="Post-Activity Reflection"
@@ -210,7 +210,12 @@ export default defineComponent({
       ...loading(save, 'Saved Successfully', 'Could not save at this time'),
       index,
       programDoc,
-      save
+      save,
+      group,
+      required,
+      deliverable,
+      endEarly,
+      minimumHours
     };
   }
   // setup() {
